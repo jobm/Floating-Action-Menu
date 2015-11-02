94cf6922b3f063547a355938232daa42ff1a37e2
JobM <jobmwaniki18@gmail.com>
2015-11-02 18:31:47 +0300

    layout reusable on all pages

--- app/src/main/java/com/job/floatingactionmenu/ViewOne.java
+++ app/src/main/java/com/job/floatingactionmenu/ViewOne.java
@@ -1,9 +1,13 @@
 package com.job.floatingactionmenu;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import com.github.clans.fab.FloatingActionButton;
 
 public class ViewOne extends AppCompatActivity {
 
@@ -11,6 +15,21 @@ public class ViewOne extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_one);
+        FloatingActionButton btn = (FloatingActionButton)findViewById(R.id.fab1);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getBaseContext(), FloatingMenuActivity.class));
+            }
+        });
+
+        FloatingActionButton btn2 = (FloatingActionButton)findViewById(R.id.fab2);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getBaseContext(), ViewTwo.class));
+            }
+        });
     }
 
     @Override
