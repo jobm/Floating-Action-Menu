5b0b63c44b583cad6a1b48385b39b5a07a2e055d
JobM <jobmwaniki18@gmail.com>
2015-11-02 17:40:31 +0300

    floating menu layout working

--- app/src/main/java/com/job/floatingactionmenu/ViewTwo.java
+++ app/src/main/java/com/job/floatingactionmenu/ViewTwo.java
@@ -1,41 +1,22 @@
 package com.job.floatingactionmenu;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
-import com.github.clans.fab.FloatingActionButton;
-
-public class FloatingMenuActivity extends AppCompatActivity {
+public class ViewTwo extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.floating_menu);
-        FloatingActionButton btn = (FloatingActionButton)findViewById(R.id.fab1);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getBaseContext(), ViewOne.class));
-            }
-        });
-
-        FloatingActionButton btn2 = (FloatingActionButton)findViewById(R.id.fab2);
-        btn2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getBaseContext(),ViewTwo.class));
-            }
-        });
+        setContentView(R.layout.activity_view_two);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_floating_menu, menu);
+        getMenuInflater().inflate(R.menu.menu_view_two, menu);
         return true;
     }
 
